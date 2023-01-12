357c2921f7a908ff0f8722c5943c64fe4dee8e84
Hiago Grades <hiagogrades@hotmail.com>
2023-01-11 22:22:03 -0300

    Inicia e finaliza o projeto

---
+++ PercorrendoArray/PercorrendoArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace PercorrendoArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[2];
+            meuArray[0] = 12;
+
+            Console.WriteLine(meuArray.Length);
+
+            for (var index = 0; index < meuArray.Length; index++)
+            {
+                Console.WriteLine(meuArray[index]);
+            }
+        }
+    }
+}
